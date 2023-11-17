1b48609bc88cc0535d1b251eda940a30d8f41c88
Yusuboy <Yuusufab@outlook.com>
2023-11-17 09:26:25 +0200

    changed the schema

--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 from app import app
 
-from flask import render_template, request, redirect, url_for, flash
-from user import login
+from flask import render_template, request, redirect, url_for
+from user import login, new_user
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -19,6 +19,18 @@ def index():
     return render_template('index.html', title='HR-Web-APP', notification=notification)
 
 
-@app.route('/register', methods=['GET'])
-def register_page():
-    return render_template('register.html', title='Register')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    registration_status = None
+
+    if request.method == 'POST':
+        new_username = request.form.get('new_username')
+        new_password = request.form.get('new_password')
+
+        if new_user(new_username, new_password):
+            registration_status = 'Registration successful! You can now log in.'
+            # Optionally, you might want to redirect the user to the login page here
+        else:
+            registration_status = 'Registration failed. Please choose a different username or provide a longer password.'
+
+    return render_template('register.html', title='Register', registration_status=registration_status)
